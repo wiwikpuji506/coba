3a65fab57b4c60c697a0795fd4d5f647db9b4c1e
wiwikpuji506 <159878314+wiwikpuji506@users.noreply.github.com>
2024-02-13 14:38:18 +0700

    Delete koneksi.php

--- koneksi.php
+++
@@ -1,7 +0,0 @@
-<?php 
-    $koneksi = mysqli_connect("localhost","root","","laporwiwikpl");
- 
-    if (mysqli_connect_error()){
-        echo "Koneksi database gagal : " . mysqli_connect_error();
-    }
-?>
